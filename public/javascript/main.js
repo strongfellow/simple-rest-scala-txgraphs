d66cfe774d9e1e2cf9c05ea2c21b0e3bcf1481bb
strongfellow <a@b.com>
2015-06-14 14:41:51 -0700

    stuff looking good

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,21 +1,24 @@
-requirejs(['d3', 'metricsgraphics'], function(d3, mg) {
+requirejs(['d3', 'metricsgraphics', 'jquery-ui'], function(d3, mg, jqui) {
 
-  $(d3.json('/metrics/transactions', function(data) {
-    console.log(data);
-    var points = data.datapoints;
-    for (var i = 0; i < points.length; i++) {
-      points[i].date = new Date(points[i].timestamp);
-    }
-    console.log(points);
-    MG.data_graphic({
-      title: "Downloads",
-      description: "This graphic shows a time-series of downloads.",
-      data: points,
-      width: 600,
-      height: 250,
-      target: '#transactions',
-      x_accessor: 'date',
-      y_accessor: 'sum',
-    })
-  }));
+  $(function() {
+
+    $("#startDate").datepicker();
+
+    d3.json('/metrics/transactions', function(data) {
+      var points = data.datapoints;
+      for (var i = 0; i < points.length; i++) {
+        points[i].date = new Date(points[i].timestamp);
+      }
+      MG.data_graphic({
+        title: "Transactions",
+        description: "This graphic shows a time-series of downloads.",
+        data: points,
+        width: 600,
+        height: 250,
+        target: '#transactions',
+        x_accessor: 'date',
+        y_accessor: 'sum',
+      });
+    });
+  });
 });
